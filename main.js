9768e8b74c84efe76947e8a610207064c0e2e458
IVAR AHTSERHS <ravistha869@gmail.com>
2023-01-19 07:54:39 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("hello");
+      }
+      if (inBetween) checkbox.checked = true;
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
